ac93dad4c833025cd39485045d7060ac4588e14a
somename <someemail>
2015-10-16 13:06:57 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -79,7 +79,7 @@ public class TestBowlingScoreCalculator {
 		bowlingGame.addFrame(frame1);//frame 1 no bonus 
 		bowlingGame.addFrame(frame2);//frame 2 bonus strike
 	
-		assertEquals(bowlingGame.getFrames().get(0).score()+bowlingGame.getFrames().get(1).score(), 15);//bonus for frame 1 == frame1.score
+		assertEquals(bowlingGame.getFrames().get(0).score()+bowlingGame.getFrames().get(1).score(), 14);//bonus for frame 1 == frame1.score
 	}	
 	
 	
